62504d7dcbb6d140cc87adffae054e3ac80f75ff
Hivenet-Nolan <144666332+Hivenet-Nolan@users.noreply.github.com>
2024-06-14 17:47:21 +0200

    Init many message pattern

--- src/modules/user.controller.ts
+++ src/modules/user.controller.ts
@@ -1,13 +1,19 @@
 import { Controller } from '@nestjs/common';
-import { UserService } from './user.service';
 import { MessagePattern } from '@nestjs/microservices';
+import { UserService } from './user.service';
+import { User } from './user.interface';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @MessagePattern({ cmd: 'getUsers' })
-  getUsers(): string {
-    return this.userService.getUsers();
+  @MessagePattern({ cmd: 'getMe' })
+  async getMe(userId: string): Promise<User> {
+    return await this.userService.getMe(userId);
+  }
+
+  @MessagePattern({ cmd: 'getUserProfile' })
+  async getUserProfile(userId: string): Promise<User> {
+    return this.userService.getUserProfile(userId);
   }
 }
--- src/modules/user.service.ts
+++ src/modules/user.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.interface';
 
 @Injectable()
 export class UserService {
   constructor() {}
 
-  getUsers(): string {
-    return 'Hello World!';
+  async getMe(userId: string): Promise<User> {
+    return {
+      id: userId,
+      name: 'test',
+      email: 'test@test.com',
+      createdAt: new Date(),
+    };
+  }
+
+  async getUserProfile(userId: string): Promise<User> {
+    return {
+      id: userId,
+      name: 'test',
+      email: 'test@test.com',
+      createdAt: new Date(),
+    };
   }
 }
