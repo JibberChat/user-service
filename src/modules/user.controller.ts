6b0bcc4ec494b889cb17309f970dcf53b28694c6
Hivenet-Nolan <144666332+Hivenet-Nolan@users.noreply.github.com>
2024-06-18 09:38:25 +0200

    Update

--- src/modules/user.controller.ts
+++ src/modules/user.controller.ts
@@ -1,19 +1,25 @@
-import { Controller } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
-import { UserService } from './user.service';
-import { User } from './user.interface';
+import { Controller } from "@nestjs/common";
+import { MessagePattern } from "@nestjs/microservices";
+
+import { User } from "./user.interface";
+import { UserService } from "./user.service";
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @MessagePattern({ cmd: 'getMe' })
+  @MessagePattern({ cmd: "getMe" })
   async getMe(userId: string): Promise<User> {
     return await this.userService.getMe(userId);
   }
 
-  @MessagePattern({ cmd: 'getUserProfile' })
+  @MessagePattern({ cmd: "getUserProfile" })
   async getUserProfile(userId: string): Promise<User> {
     return this.userService.getUserProfile(userId);
+  }
+
+  @MessagePattern({ cmd: "updateUser" })
+  async updateUser(userId: string, name: string, email: string): Promise<User> {
+    return this.userService.updateUser(userId, name, email);
   }
 }
--- src/modules/user.service.ts
+++ src/modules/user.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
-import { User } from './user.interface';
+import { Injectable } from "@nestjs/common";
+
+import { User } from "./user.interface";
 
 @Injectable()
 export class UserService {
@@ -8,8 +9,8 @@ export class UserService {
   async getMe(userId: string): Promise<User> {
     return {
       id: userId,
-      name: 'test',
-      email: 'test@test.com',
+      name: "test",
+      email: "test@test.com",
       createdAt: new Date(),
     };
   }
@@ -17,8 +18,17 @@ export class UserService {
   async getUserProfile(userId: string): Promise<User> {
     return {
       id: userId,
-      name: 'test',
-      email: 'test@test.com',
+      name: "test",
+      email: "test@test.com",
+      createdAt: new Date(),
+    };
+  }
+
+  async updateUser(userId: string, name: string, email: string): Promise<User> {
+    return {
+      id: userId,
+      name: name,
+      email: email,
       createdAt: new Date(),
     };
   }
